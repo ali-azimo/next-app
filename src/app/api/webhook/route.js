8aed67254555c663e09f10c0e95ea7766b75066c
Ali Azimo <webdesign.aejl@gmail.com>
2024-12-31 02:25:51 +0200

    resume

--- src/app/api/webhook/route.js
+++ src/app/api/webhook/route.js
@@ -52,7 +52,6 @@ export async function POST(req) {
   const { id } = evt?.data
   const eventType = evt?.type
 
-
     if(eventType === 'user.created' || eventType === 'user.updated'){
       const { id, first_name, last_name, image_url, email_addresses } = evt?.data;
       try{
